f446f0fa2193f3e5ffe881f85455c8266561acdb
Julia15Lop <julialopezs15@hotmail.com>
2021-02-17 22:38:47 +0100

    Terraform improves

--- terraform/modules/vms/user_data/worker01.sh
+++ terraform/modules/vms/user_data/worker01.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -xe
 
-# Update system
-sudo yum update -y
-
 # Set hostname
 HOST_PREFIX=worker01
 sudo hostnamectl set-hostname $HOST_PREFIX.azure
@@ -15,7 +12,4 @@ sudo timedatectl set-timezone Europe/Madrid
 sudo timedatectl set-ntp true
 
 # Disable SELinux
-sed -i s/=enforcing/=disabled/g /etc/selinux/config
-
-# Reboot system
-sudo reboot
+sudo sed -i s/=enforcing/=disabled/g /etc/selinux/config
